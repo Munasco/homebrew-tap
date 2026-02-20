1ab27ec49dfcf9f1f8393eff8b12b326108d6178
Munachi Ernest-Eze <ernest-eze1869@loyolajesuit.org>
2026-02-20 03:44:43 -0500

    torvax v0.8.0 — add macOS ARM binary

--- Formula/torvax.rb
+++ Formula/torvax.rb
@@ -7,18 +7,7 @@ class Torvax < Formula
   on_macos do
     on_arm do
       url "https://github.com/Munasco/torvax/releases/download/v0.8.0/torvax-v0.8.0-aarch64-apple-darwin.tar.gz"
-    end
-    on_intel do
-      url "https://github.com/Munasco/torvax/releases/download/v0.8.0/torvax-v0.8.0-x86_64-apple-darwin.tar.gz"
-    end
-  end
-
-  on_linux do
-    on_arm do
-      url "https://github.com/Munasco/torvax/releases/download/v0.8.0/torvax-v0.8.0-aarch64-unknown-linux-gnu.tar.gz"
-    end
-    on_intel do
-      url "https://github.com/Munasco/torvax/releases/download/v0.8.0/torvax-v0.8.0-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "512b03c4b0de3064ed83d42f06f76cfa3f88fb30d80e553fe9f0320feaeb412f"
     end
   end
 
