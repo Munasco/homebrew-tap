a6c41016a0dfeb35cdbad3e2829c33cbc546ea15
Munachi Ernest-Eze <ernest-eze1869@loyolajesuit.org>
2026-02-20 04:25:55 -0500

    torvax v0.1.0

--- Formula/torvax.rb
+++ Formula/torvax.rb
@@ -1,13 +1,13 @@
 class Torvax < Formula
   desc "Git review of your diffs, like a movie"
   homepage "https://github.com/Munasco/torvax"
-  version "0.8.0"
+  version "0.1.0"
   license "ISC"
 
   on_macos do
     on_arm do
-      url "https://github.com/Munasco/torvax/releases/download/v0.8.0/torvax-v0.8.0-aarch64-apple-darwin.tar.gz"
-      sha256 "a174d71b1da29ed6ed64dc1a3e1ed9c2bc15036502e0eec513197ab04013d59f"
+      url "https://github.com/Munasco/torvax/releases/download/v0.1.0/torvax-v0.1.0-aarch64-apple-darwin.tar.gz"
+      sha256 "0d6c946d61f7bae042661671bac10bea8ffd775a74fce6cacc335748e1d888be"
     end
   end
 
